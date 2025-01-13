05a5478c14be85165284a1f96b3849ff27787dd1
Redbeard93 <67786490+Redbeard93@users.noreply.github.com>
2025-01-13 14:27:29 +0800

    Update Microsoft.PowerShell_profile.ps1

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,23 +1,2 @@
 $ENV:STARSHIP_CONFIG = "$HOME\Documents\PowerShell\starship.toml"
 Invoke-Expression (&starship init powershell)
-$ENV:LFCD = "$HOME\Documents\PowerShell\lfcd.ps1"
-
-     Set-PSReadLineKeyHandler -Chord Ctrl+o -ScriptBlock {
-         [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
-         [Microsoft.PowerShell.PSConsoleReadLine]::Insert('.$ENV:LFCD')
-         [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
-     }
-
-     Set-PSReadLineKeyHandler -Chord F1 -ScriptBlock {
-         [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
-         [Microsoft.PowerShell.PSConsoleReadLine]::Insert('wsl -d archlinux')
-         [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
-
-     }
-
-     Set-PSReadLineKeyHandler -Chord F2 -ScriptBlock {
-         [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
-         [Microsoft.PowerShell.PSConsoleReadLine]::Insert('wsl -t archlinux')
-         [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
-
-     }
